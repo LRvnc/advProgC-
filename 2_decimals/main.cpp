5e7525e52e68b608258af5c23545a6b6e525133a
Leandro <rissov.leandro@gmail.com>
2023-01-15 14:24:27 +0100

    decimals v1

--- 2_decimals/main.cpp
+++ 2_decimals/main.cpp
@@ -1,78 +1,105 @@
 #include <iostream>
 #include <cstring>
 
-bool is_finished (int *p, int N) {
+bool is_finished (int *p, int N)
+{
 
     int i;
 
-    for (i = 0; i < N; i++){
-        if (p[i] != 0) {
+    for (i = 0; i < N; i++)
+    {
+        if (p[i] != 0)
+        {
             return false;
         }
     }
     return true;
 }
 
-void table_sum (int *p, int *k, int N) {
+void table_sum (int *p, int *k, int N)
+{
     // Sum of tables *p and *k, stores in table *p, with dimension N
 
     int i;
     int elements_sum, value_pass = 0;
 
-    for (i = N-1; i >= 0; i--) {
+    for (i = N-1; i >= 0; i--)
+    {
         elements_sum = p[i] + k[i] + value_pass;
         p[i] = elements_sum % 1000; // remainder is saved
         value_pass = elements_sum / 1000;
     }
 }
 
-void print_dyn_table (int *p, unsigned int N) {
+void table_divide (int *p, unsigned int l, unsigned int N)
+{
+    // Divide table *p by l. Stores in table *p, with dimension N
+
+    unsigned int i;
+    int elements_div, value_pass = 0;
+
+    for (i = 0; i < N; i++)
+    {
+        elements_div = p[i] + value_pass*1000;
+        p[i] = elements_div / l; 
+        value_pass = elements_div % l; // remainder is saved
+    }
+
+}
+
+void print_dyn_table (int *p, unsigned int N)
+{
 
     unsigned int i = 0;
 
-    for (i = 0; i < N; i++) {
-        if (p[i] < 10) {
+    for (i = 0; i < N; i++)
+    {
+        if (p[i] < 10)
+        {
             std::cout << "00" << p[i];
         }
-        else if (p[i] < 100) {
+        else if (p[i] < 100)
+        {
             std::cout << "0" << p[i];
         }
-        else {
+        else
+        {
             std::cout << p[i];
         }        
-        std::cout << " ";
+        // std::cout << " ";
     }
     std::cout << std::endl;
-
 }
 
 int main (void) {
 
-    unsigned int k=0;
+    unsigned int n = 0, k = 3;
 
     // Number of decimals    
     std::cout << "Please enter an integer value: ";
-    std::cin >> k;
+    std::cin >> n;
+
+    n /= 3;
 
     // Creating table (dyn. allocation)
-    int *vec1 = new int [k];
-    int *vec2 = new int [k];
+    int *decimals_e = new int [n];
+    int *aux_table = new int [n];
 
-    memset(vec1, 0, k*sizeof(int));
-    memset(vec2, 0, k*sizeof(int));
+    memset(decimals_e, 0, n*sizeof(int));
+    memset(aux_table, 0, n*sizeof(int));
 
-    vec1[k-1] = 551;
-    vec1[k-2] = 650;
-    vec1[k-3] = 1;
-    print_dyn_table(vec1, k);
-    vec2[k-1] = 650;
-    vec2[k-2] = 750;
-    vec2[k-3] = 3;
-    print_dyn_table(vec2, k);
+    // Init aux_table
+    aux_table[0] = 500; // (1/2 = 0.5)
 
-    table_sum(vec1, vec2, k);
-    print_dyn_table(vec1, k);
+    while (not is_finished(aux_table, n))
+    {
+        table_sum(decimals_e, aux_table, n);
+        table_divide(aux_table, k, n);
+        k++;
+    }
 
+    print_dyn_table(decimals_e, n);
+    
     // main return
     return 0;
 }
